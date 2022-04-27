012943e8d1499fa6de2be63e34b5ce65a966bc6b
Vanni <bielvanni@hotmail.com>
2022-04-27 05:32:36 -0300

    2nd commit
    Parte Final Módulo 3, capitulo 5.

--- gatitobook/src/app/animais/animais.module.ts
+++ gatitobook/src/app/animais/animais.module.ts
@@ -7,9 +7,19 @@ import { AnimalComponent } from './animal/animal.component';
 import { CartaoModule } from '../componentes/cartao/cartao.module';
 import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-animais.component';
 import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
+import { ComentariosComponent } from './detalhe-animal/comentarios/comentarios.component';
+import { MensagemModule } from '../componentes/mensagem/mensagem.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [ListaAnimaisComponent, AnimalComponent, GradeFotosAnimaisComponent, DetalheAnimalComponent],
-  imports: [CommonModule, AnimaisRoutingModule, CartaoModule],
+  declarations: [
+    ListaAnimaisComponent,
+    AnimalComponent,
+    GradeFotosAnimaisComponent,
+    DetalheAnimalComponent,
+    ComentariosComponent,
+  ],
+  imports: [CommonModule, AnimaisRoutingModule, CartaoModule, SharedModule],
 })
 export class AnimaisModule {}
